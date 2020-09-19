352f3b64c4c0816fee20f0560c198918501aced0
kochetkov0 <v.kochetkov@luxystesh.com>
2020-09-19 16:40:55 -0400

    initial commit

---
+++ __init__.py
@@ -0,0 +1,22 @@
+import sys, socket, threading, argparse
+
+
+# global variables
+
+listen = False
+command = False
+upload = False
+execute = ''
+target = ''
+upload_destination = ''
+port = 0
+
+
+
+def main():
+    
+    return 0
+
+
+if __name__ == "__main__":
+    main()
